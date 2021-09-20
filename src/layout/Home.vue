b73c5a04652e882d4d05860546982c515259bf04
ALIESTRADA <aestrada67@hotmail.com>
2021-09-20 13:02:30 +0200

    Name Fixed

--- src/layout/Home.vue
+++ src/layout/Home.vue
@@ -1,27 +1,5 @@
 <template>
-    <div>
-      <h1>Clinica veterinaria</h1>
-    </div>
-    <!-- Aquí empieza el header -->
-
-    <!-- Aquí termina el header -->
-
-
-    <!-- Aquí empiezan los servicios -->
-
-    <!-- Aquí terminan los servicios -->
-
-
-    <!-- Aquí empiezan los productos -->
-
-    <!-- Aquí terminan los productos -->
-
-
-    <!-- Aquí empieza el footer -->
-
-    <!-- Aquí termina el footer -->
-
-
+  
 </template>
 
 <script>
