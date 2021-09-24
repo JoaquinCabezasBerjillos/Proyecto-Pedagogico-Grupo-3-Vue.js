97f25ac1100179259441773a4121f3fdda8ceb38
unknown <joaquincabezasberjillos@gmail.com>
2021-09-24 10:50:29 +0200

    productos

--- src/main.js
+++ src/main.js
@@ -4,12 +4,9 @@ import store from './store'
 import router from './router'
 window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js")
 
-import "./assets/ProductosCss/style.css";
 import "./assets/ProductosCss/bootstrap.css";
 import "./assets/ProductosCss/bootstrap.min.css";
 import "./assets/ProductosCss/prettyPhoto.css";
 
-import "./assets/ProductosJs/bootstrap.js";
-import "./assets/ProductosJs/main.js";
 
 createApp(App).use(router).use(store).mount('#app')
--- src/views/Productos.vue
+++ src/views/Productos.vue
@@ -6,52 +6,39 @@
       <hr>
       <div class="clearfix"></div>
     </div>
-    <div class="categories">
-      <!-- <ul class="cat">
-         <li>
-          <ol class="type">
-            <li><a href="#" data-filter="*" class="active">All</a></li>
-            <li><a href="#" data-filter=".web">Web Design</a></li>
-            <li><a href="#" data-filter=".app">App Development</a></li>
-            <li><a href="#" data-filter=".branding">Branding</a></li>
-          </ol>
-        </li> 
-      </ul> -->
-      <div class="clearfix"></div>
-    </div>
     <div class="row">
       <div class="portfolio-items">
         <div class="col-sm-3 col-md-3 col-lg-4 web">
           <div class="portfolio-item">
-            <div class="hover-bg"> <a href="./src/assets/img/Alimentacion.jpg" rel="prettyPhoto">
+            <div class="hover-bg"> <a href="../assets/img/Alimentacion.jpg" rel="prettyPhoto">
               <div class="hover-text">
                 <h4>Alimentación</h4>
                 <small>Gran variedad de alimentación para tu mascota</small>
                 <div class="clearfix"></div>
               </div>
-              <img src="./src/assets/img/Alimentacion.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+              <img src="../assets/img/Alimentacion.jpg" class="img-responsive" alt="Project Title"> </a> </div>
           </div>
         </div>
         <div class="col-sm-3 col-md-3 col-lg-4 app">
           <div class="portfolio-item">
-            <div class="hover-bg"> <a href="./src/assets/img/Medicina4.jpg" rel="prettyPhoto">
+            <div class="hover-bg"> <a href="../assets/img/Medicina4.jpg" rel="prettyPhoto">
               <div class="hover-text">
                 <h4>Medicamentos</h4>
                 <small>Para cuidar la salud de tu mascota</small>
                 <div class="clearfix"></div>
               </div>
-              <img src="./src/assets/img/Medicina4.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+              <img src="../assets/img/Medicina4.jpg" class="img-responsive" alt="Project Title"> </a> </div>
           </div>
         </div>
         <div class="col-sm-3 col-md-4 col-lg-4 web">
           <div class="portfolio-item">
-            <div class="hover-bg"> <a href="./src/assets/img/Accesorios3.jpg" rel="prettyPhoto">
+            <div class="hover-bg"> <a href="../assets/img/Accesorios3.jpg" rel="prettyPhoto">
               <div class="hover-text">
                 <h4>Accesorios</h4>
                 <small>Todo tipo de productos para tu mascota </small>
                 <div class="clearfix"></div>
               </div>
-              <img src="./src/assets/img/Accesorios3.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+              <img src="../assets/img/Accesorios3.jpg" class="img-responsive" alt="Project Title"> </a> </div>
               </div>
           </div>
         </div>
@@ -67,5 +54,5 @@ export default {
 </script>
 
 <style>
-
+@import "../assets/ProductosCss/style.css";
 </style>
