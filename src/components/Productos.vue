908958ae1d30a8487f891dad711a7ad9b0b77344
JoaquinCabezasBerjillos <joaquincabezasberjillos@gmail.com>
2021-09-28 11:51:28 +0200

    correcion de images en dev

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -10,35 +10,35 @@
       <div class="portfolio-items">
         <div class="col-sm-3 col-md-3 col-lg-4 web">
           <div class="portfolio-item">
-            <div class="hover-bg"> <a href="../assets/img/Alimentacion.jpg" rel="prettyPhoto">
+            <div class="hover-bg"> <a href="../assets/images/Alimentacion.jpg" rel="prettyPhoto">
               <div class="hover-text">
                 <h4>Alimentación</h4>
                 <small>Gran variedad de alimentación para tu mascota</small>
                 <div class="clearfix"></div>
               </div>
-              <img src="../assets/img/Alimentacion.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+              <img src="../assets/images/Alimentacion.jpg" class="img-responsive" alt="Project Title"> </a> </div>
           </div>
         </div>
         <div class="col-sm-3 col-md-3 col-lg-4 app">
           <div class="portfolio-item">
-            <div class="hover-bg"> <a href="../assets/img/Medicina4.jpg" rel="prettyPhoto">
+            <div class="hover-bg"> <a href="../assets/images/Medicina4.jpg" rel="prettyPhoto">
               <div class="hover-text">
                 <h4>Medicamentos</h4>
                 <small>Para cuidar la salud de tu mascota</small>
                 <div class="clearfix"></div>
               </div>
-              <img src="../assets/img/Medicina4.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+              <img src="../assets/images/Medicina4.jpg" class="img-responsive" alt="Project Title"> </a> </div>
           </div>
         </div>
         <div class="col-sm-3 col-md-4 col-lg-4 web">
           <div class="portfolio-item">
-            <div class="hover-bg"> <a href="../assets/img/Accesorios3.jpg" rel="prettyPhoto">
+            <div class="hover-bg"> <a href="../assets/images/Accesorios3.jpg" rel="prettyPhoto">
               <div class="hover-text">
                 <h4>Accesorios</h4>
                 <small>Todo tipo de productos para tu mascota </small>
                 <div class="clearfix"></div>
               </div>
-              <img src="../assets/img/Accesorios3.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+              <img src="../assets/images/Accesorios3.jpg" class="img-responsive" alt="Project Title"> </a> </div>
               </div>
           </div>
         </div>
