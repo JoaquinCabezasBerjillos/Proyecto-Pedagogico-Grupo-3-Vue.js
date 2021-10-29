96962551ea3083cd3fe4986bfb02fe0f52375323
JoaquinCabezasBerjillos <joaquincabezasberjillos@gmail.com>
2021-10-29 08:50:58 +0200

    vista usuario

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "../views/Home.vue";
 import AdminLayout from "../layout/Admin.vue";
+import User from "../layout/User.vue";
 import Consulta from "../views/Consulta.vue";
 import Productos from "../views/Productos.vue";
 import Pedidos from "../views/Pedidos.vue";
@@ -10,6 +11,7 @@ import Register from "../views/Register.vue";
 import Reset from "../views/Reset.vue";
 import Mascotas from "../views/Mascotas.vue";
 import Clientes from "../views/Clientes.vue";
+import Usuarios from "../views/Usuarios.vue";
 import NotFound from "../views/NotFound.vue";
 
 
@@ -20,7 +22,20 @@ const routes = [
     name: "Home",
     component: Home,
   },
+  {
+    path: "/user",
+    name: "User",
+    component: User,
+    meta: { requiresAuth: true},
 
+    children: [
+      {
+        path: "/usuarios",
+        name: "Usuarios",
+        component: Usuarios,
+      },
+    ]
+  },
   {
     // ruta protegida
     path: "/admin",
@@ -64,8 +79,6 @@ const routes = [
       
       ]
   }, 
-
-   
 
      {
     path: "/login",
